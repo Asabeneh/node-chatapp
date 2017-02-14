3e790edb2956068b26d59b20fc0b4b9518f600ce
Asabeneh <asabeneh@gmail.com>
2017-02-14 11:57:23 +0200

    new createMessage added

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,8 @@
 var socket = io();
 socket.on('connect',function(){
   console.log('Connected to server');
-  socket.emit('createMessage',{
-    from:"Asabeneh",
-    text:"Yeb, it works for me"
-  })
+
+
 });
 socket.on('disconnect',function(){
   console.log('Disconnected from server');
--- server/server.js
+++ server/server.js
@@ -14,13 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 
 console.log('New user connected');
-socket.emit('newMessage',{
-  from:'John',
-  text:'see you then',
-  createdAt:1213123
-});
 socket.on('createMessage',(message) =>{
-  console.log('createMessage',message)
+  console.log('createMessage',message);
+  io.emit('newMessage',{
+    from:message.from,
+    text:message.text,
+    createdAt:new Date().getTime()
+  })
 })
 socket.on('disconnect',function(){
   console.log('User was disconnected');
