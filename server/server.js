365e5143c7002184d5387c277c927e372afc6bbc
Asabeneh <asabeneh@gmail.com>
2017-02-14 04:21:30 +0200

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const http =require('http');
 const path = require('path');
+const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-
+const port = process.env.PORT || 3000;
 var app = express();
+
 app.use(express.static(publicPath));
-app.listen(3000,()=>{
-  console.log(`The sever is running on 3000`)
-})
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
