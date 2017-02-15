310a8717bfd1ad7739cabbd58fc6c6ae85ffb892
Asabeneh <asabeneh@gmail.com>
2017-02-15 06:04:31 +0200

    autoscroll added

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBottom(){
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=scrollHeight){
+  messages.scrollTop(scrollHeight);
+
+}
+
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -17,6 +35,7 @@ socket.on('newMessage', function (message) {
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
   // var formattedTime = moment(message.createdAt).format('h:mm a');
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -29,9 +48,10 @@ var template = jQuery('#location-message-template').html();
 var html = Mustache.render(template,{
   from:message.from,
   url:message.url,
-  createdAt:fromattedTime
+  createdAt:formattedTime
 });
 jQuery('#messages').append(html);
+  scrollToBottom();
   //
   // var formattedTime = moment(message.createdAt).format('h:mm a');
   // var li = jQuery('<li></li>');
