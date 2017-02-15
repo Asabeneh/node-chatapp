2e1e27c7ad0c090bd2532181e6ed0ed9e339b553
Asabeneh <asabeneh@gmail.com>
2017-02-15 03:47:45 +0200

    css added for the chatapp

--- public/js/index.js
+++ public/js/index.js
@@ -25,11 +25,13 @@ socket.on('newLocationMessage',function(message){
 });
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+  var messageTextBox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
+    messageTextBox.val('');
 
   });
 });
@@ -40,7 +42,12 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
+    locationButton.attr('disabled','disabled').text('sending location...');
+
+
   navigator.geolocation.getCurrentPosition(function (position) {
+
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
