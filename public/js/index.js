8670e88895b51a8c122b8648f54863b61182baff
Asabeneh <asabeneh@gmail.com>
2017-02-15 03:13:50 +0200

    Addd geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,14 @@ socket.on('newMessage', function (message) {
 
   jQuery('#messages').append(li);
 });
-
+socket.on('newLocationMessage',function(message){
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target = "_blank">My Current Location</a>');
+  li.text(`${message.from}:`);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage,generateLocationMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -26,7 +27,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`));
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
