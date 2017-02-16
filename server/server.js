ff40b6b87e4cf9b0a8b19f0c30235c6f2cc07163
Asabeneh <asabeneh@gmail.com>
2017-02-16 04:32:06 +0200

    Send messages to only people in the same room

--- server/server.js
+++ server/server.js
@@ -44,13 +44,23 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
+    var user = users.getUser(socket.id);
+    if(user && isRealString(message.text)){
+        io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+
+    }
     console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    if(user){
+        io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+
+    }
+
   });
 
   socket.on('disconnect', () => {
@@ -64,4 +74,6 @@ io.on('connection', (socket) => {
   });
 });
 
-console.log("The server is running")
+server.listen(port,()=>{
+  console.log(`The server is runnin on port ${port}`);
+})
